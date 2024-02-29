140792e4c5a3b3dbda6028f29700527aae84e886
Austen Turner <austenpturner@msn.com>
2024-02-29 14:13:54 -0800

    add dark and light mode js logic

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,41 @@
-console.log('Hello');
+const toggle = document.querySelector('.toggle__wrapper');
+const darkBtn = document.getElementById('dark');
+const lightBtn = document.getElementById('light');
+
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark';
+    localStorage.setItem('colorMode', 'dark');
+};
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light';
+    localStorage.setItem('colorMode', 'light');
+};
+
+const getColorModeFromLocalStorage = () => {
+    return localStorage.getItem('colorMode');
+};
+
+const getColorModeFromPreferences = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches 
+        ? 'dark' 
+        : 'light';
+};
+
+const setColorMode = () => {
+    const color = getColorModeFromLocalStorage() || getColorModeFromPreferences();
+    color === 'dark' 
+        ? darkBtn.click()
+        : lightBtn.click();
+};
+
+toggle.addEventListener('click', () => {
+    darkBtn.checked ? setDarkMode() : setLightMode();
+});
+
+window.matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', (e) => {
+            e.matches ? darkBtn.click() : lightBtn.click();
+        });
+
+setColorMode();
